f685c55dee34d159a8841c1c9b43366051765de4
Álvaro Hernández Colino <alvarohcolino@gmail.com>
2024-01-16 09:00:33 +0100

    First commit (initial files)

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+const { src, dest, watch , series, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('autoprefixer');
+const postcss    = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const cssnano = require('cssnano');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin'); // Minify images
+const notify = require('gulp-notify');
+const cache = require('gulp-cache');
+const clean = require('gulp-clean');
+const webp = require('gulp-webp');
+
+const paths = {
+    scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js',
+    images: 'src/img/**/*'
+}
+
+function css() {
+    return src(paths.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        // .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe( dest('public/build/css') );
+}
+
+
+function javascript() {
+    return src(paths.js)
+      .pipe(terser())
+      .pipe(sourcemaps.write('.'))
+      .pipe(dest('public/build/js'));
+}
+
+function images() {
+    return src(paths.images)
+        .pipe(cache(imagemin({ optimizationLevel: 3})))
+        .pipe(dest('public/build/img'))
+        .pipe(notify({ message: 'Imagen Completada'}));
+}
+
+function webpVersion() {
+    return src(paths.images)
+        .pipe( webp() )
+        .pipe(dest('public/build/img'))
+        .pipe(notify({ message: 'Imagen Completada'}));
+}
+
+
+function watchFiles() {
+    watch( paths.scss, css );
+    watch( paths.js, javascript );
+    watch( paths.images, images );
+    watch( paths.images, webpVersion );
+}
+  
+exports.css = css;
+exports.watchFiles = watchFiles;
+exports.default = parallel(css, javascript,  images, webpVersion,  watchFiles ); 
